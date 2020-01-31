ad6afad556128b77f9ff0f83630452a0d2591aff
TStasio <60015229+TStasio@users.noreply.github.com>
2020-01-31 16:39:52 -0500

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,15 @@
+"""
+Author: Tanya Stasio
+Created: 01/21/2020
+Note: Python 3.4 used
+"""
+#Lets calc the area and perimeter of a circle; assign values for pi, radius, area, perimeter
+pi=3.14
+r=2.0
+area=pi*(r**2)
+perim=2.0*pi*r
+
+#Output results
+print('Given r:', r)
+print ('Area is:', area)
+print ('Perimeter is:', perim)
